b1b43810d7126257c7e76dbf38fbed9267e08420
kush Muhamad <kushmohammed98@yahoo.com>
2023-05-18 13:03:31 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
 require 'pry'
 
-class String
+class String 
+
+  def initialize(string)
+    @string = string
+  end
 
   def sentence?
+    self.end_with?(".")
+
+
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    
 
   end
 end
